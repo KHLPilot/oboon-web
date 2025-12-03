28b5be430bdc56e6f9b6d0ed1f90abedf7e42457
hyosu101 <96734308+hyosu101@users.noreply.github.com>
2025-12-03 12:43:29 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
                 <div className="text-xl font-bold">오분: 오늘의 분양</div>
                 <div className="text-xs text-slate-400">
                   어려운 분양 정보도 오분이면 돼~
+                  업로드 테스트
                 </div>
               </Link>
             </nav>
